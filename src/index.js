59abc03ffdd638e88287723906ff8f71d34c00fc
Luis <luiyi_salazar@hotmail.com>
2023-12-29 13:04:38 -0600

    Set ternary operator for the menu and the footer-schedule time

--- src/index.js
+++ src/index.js
@@ -66,15 +66,21 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu:</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : null}
       {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
@@ -106,14 +112,23 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 8;
+  const openHour = 11;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
   return (
     <footer className="footer">
-      We are currently open! - {new Date().toLocaleTimeString()}
+      {isOpen ? (
+        <div className="order">
+          <p>We are open until {closeHour}:00!!</p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>
+          We will be back between {openHour}:00 and {closeHour}:00!
+        </p>
+      )}
     </footer>
   );
 }
