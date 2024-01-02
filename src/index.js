cc13028289ce39174195a2f5f33565db26e33cca
Luis <luiyi_salazar@hotmail.com>
2024-01-02 11:53:34 -0600

    Set Text and Class conditional for the Sold-out menu

--- src/index.js
+++ src/index.js
@@ -104,14 +104,14 @@ function Menu() {
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
       </div>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
     </li>
